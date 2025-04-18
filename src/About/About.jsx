a1cd89612240315cadb409a246e95f96a2b6bb85
YK-1992 <179270256+YK-1992@users.noreply.github.com>
2025-04-18 15:07:19 +0200

    motion animation

--- src/About/About.jsx
+++ src/About/About.jsx
@@ -1,4 +1,5 @@
 import './about.scss'
+import { motion } from 'framer-motion';
 const About = () => {
     const loadCV = () => {
         window.open("./CV YKhambir.pdf");
@@ -7,26 +8,52 @@ const About = () => {
         <>
         <div className="container-about" id='about'>
             <div className="about-picture-section">
-            <div className="photo-block">
+            <motion.div 
+                       initial={{opacity: 0.2, x: -100, scale: 0.8}}
+                       whileInView={{opacity: 4, x: 0, scale: 1}}
+                       transition={{duration: 3, }}
+                       viewport={{ once: true, amount: 0.2 }}
+            className="photo-block">
+
                 <img src="./photo/profphoto.png" alt="" />         
-            </div>
-            <div className='border-line'>
+            </motion.div>
+            <motion.div 
+             initial={{opacity: 0.2, x: 100, scale: 0.8}}
+             whileInView={{opacity: 4, x: 0, scale: 1}}
+             transition={{duration: 3, }}
+             viewport={{ once: true, amount: 0.2 }}
+            
+            
+            className='border-line'>
                 <div className='border-section'></div>
-            </div>
+            </motion.div>
             </div>
           
             <div className="about-section">
-                <h1>About Me.</h1>
-                <div className='box-style'></div>
+                <motion.div 
+                  viewport={{ once: true, amount: 0.5 }}
+                  initial={{ opacity: 1, x: -100, scale: 0.1 }}
+                  whileInView={{ opacity: 1, x: 0, scale: 1 }}
+                  transition={{ duration: 3 }}
+                className="div">
+
+<h1
+                   
+                   >About Me.</h1>
+                   <div 
+                     
+                    className='box-style'></div>
+                </motion.div>
+           
                 <p> &mdash; Frontend Developer specializing in React.js and JavaScript,
                      with hands-on experience in Redux Toolkit and Material-UI. 
                     I excel at building responsive, user-centric web applications and 
                     am keen on optimizing performance and enhancing usability.
                     Currently engaged in real-world projects, I am eager to contribute 
-                    to a team that values innovation, continuous learning, and qual</p>
+                    to a team that values innovation, continuous learning, and qual.</p>
                     <p>
                    <span>  &mdash; My Skills Are: </span>  
-                    HTML, CSS, SCSS, JavaScript, Version Control (Git), React JS, Material UI, Bootstrap, API, Redux, TypeScrip
+                    HTML, CSS, SCSS, JavaScript, Version Control (Git), React JS, Material UI, Bootstrap, API, Redux, TypeScrip, WordPress.
                     </p>
 
                     <button className='button-CV' onClick={loadCV}>Download CV</button>
--- src/Introduction/Introduc.jsx
+++ src/Introduction/Introduc.jsx
@@ -2,7 +2,27 @@ import "./style-introduc.scss"
 import Links from '../contacts.json'
 import HeaderBlock from "../Header/Header"
 import { motion } from "motion/react"
+
+const name = "I`M YAROSLAV KHAMBIR";
+const container = {
+    hidden: { opacity: 0 },
+    visible: {
+      opacity: 1,
+      transition: {
+        staggerChildren: 0.1, 
+        delayChildren: 0.3,   
+      },
+    },
+  };
+  const letter = {
+    hidden: { opacity: 0, y: 20 },
+    visible: { opacity: 1, y: 0, transition: { duration: 0.3 } },
+  };
+  
+
+
 const Introduction = () => {
+
     return (
 <>
 <div className="container-introduction" id="home">
@@ -13,7 +33,21 @@ const Introduction = () => {
 
     <div className="name-section">
         <span>Hallo</span>
-        <h1>I`M YAROSLAV KHAMBIR!</h1>
+        <motion.div className="name-style"
+        variants={container}
+        initial="hidden"
+        animate="visible"
+        style={{ display: 'flex', flexWrap: 'wrap', fontSize: '3.5rem', fontWeight: 'bold' }}
+        >
+        
+        {name.split('').map((world, index) => (
+          <motion.span className="name-style" key={index} variants={letter}>
+            {world === ' ' ? '\u00A0' : world}
+          </motion.span>
+          
+        ))}
+      
+        </motion.div>
         <p> 
     <span> &mdash; FRONT END DEVELOPER,</span> with knowledge in web development and design, I offer the best projects resulting in quality work. 
         </p>
